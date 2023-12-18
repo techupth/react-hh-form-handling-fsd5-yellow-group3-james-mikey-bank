6cc06b1b972966ff3748d9bd8808480c3c1f3937
acorriaz <mikey.swx@gmail.com>
2023-12-18 22:35:54 +0700

    Add ex

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 function ProductForm() {
+  const [formData, setFormData] = useState({});
+
+  console.log("form data:", formData);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(JSON.stringify(formData));
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +29,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(e) => handleChange(e)}
           />
         </label>
       </div>
@@ -22,7 +41,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(e) => handleChange(e)}
           />
         </label>
       </div>
@@ -34,7 +53,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(e) => handleChange(e)}
           />
         </label>
       </div>
@@ -46,7 +65,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(e) => handleChange(e)}
             rows={4}
             cols={30}
           />
